ddb70c43c0b63cb5af74fb059975cac17bf9f7b9
Craig M. Stimmel <cstimmel@uw.edu>
2015-07-23 15:44:01 -0700

    Make the API read only unless authenticated.

--- mdot_rest/views.py
+++ mdot_rest/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from .models import Resource
 from .serializers import ResourceSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class ResourceList(generics.ListCreateAPIView):
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class ResourceDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
