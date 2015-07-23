a0a662ab8a70f1548d72eeba81cc7feeaaa32f2b
Craig M. Stimmel <cstimmel@uw.edu>
2015-07-23 14:59:38 -0700

    Nest relation to ResourceLink so it shows in the JSON representation.

--- mdot_rest/serializers.py
+++ mdot_rest/serializers.py
@@ -1,7 +1,16 @@
-from .models import Resource
+from .models import Resource, ResourceLink
 from rest_framework import serializers
 
 
+class ResourceLinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ResourceLink
+        fields = ('link_type', 'title', 'url')
+
+
 class ResourceSerializer(serializers.ModelSerializer):
+    resourcelink_set = ResourceLinkSerializer(many=True, read_only=True)
+
     class Meta:
         model = Resource
+        fields = ('id', 'name', 'feature_desc', 'featured', 'accessible', 'responsive_web', 'resourcelink_set', 'created_date', 'last_modified')
