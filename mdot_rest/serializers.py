baac82884e1265d036dda9897062ae464f3b6154
haughar <haughar@uw.edu>
2015-07-23 15:19:20 -0700

    Nest relation to IntendedAudience so it shows in django representation.

--- mdot_rest/serializers.py
+++ mdot_rest/serializers.py
@@ -1,16 +1,23 @@
-from .models import Resource, ResourceLink
+from .models import Resource, ResourceLink, IntendedAudience
 from rest_framework import serializers
 
 
 class ResourceLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = ResourceLink
-        fields = ('link_type', 'title', 'url')
+        fields = ('link_type', 'title', 'url',)
+
+
+class IntendedAudienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IntendedAudience
+        fields = ('name',)
 
 
 class ResourceSerializer(serializers.ModelSerializer):
     resourcelink_set = ResourceLinkSerializer(many=True, read_only=True)
+    intendedaudience_set = IntendedAudienceSerializer(many=True, read_only=True)
 
     class Meta:
         model = Resource
-        fields = ('id', 'name', 'feature_desc', 'featured', 'accessible', 'responsive_web', 'resourcelink_set', 'created_date', 'last_modified')
+        fields = ('id', 'name', 'feature_desc', 'featured', 'accessible', 'responsive_web', 'resourcelink_set', 'intendedaudience_set', 'created_date', 'last_modified',)
