b5e62824ac3b02de7a48cfeb264bcc9117db6e0e
Craig M. Stimmel <cstimmel@uw.edu>
2015-07-24 16:20:22 -0700

    Enable filtering by audience.

--- mdot_rest/views.py
+++ mdot_rest/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
-from .models import Resource
+import django_filters as filters
+from .models import Resource, IntendedAudience
 from .serializers import ResourceSerializer
 from rest_framework import generics, permissions
-import django_filters
 
 
-class ResourceFilter(django_filters.FilterSet):
+class ResourceFilter(filters.FilterSet):
+    audience = filters.CharFilter(name='intendedaudience__audience')
+
     class Meta:
         model = Resource
-        fields = ('title', 'featured', 'accessible', 'responsive_web',)
+        fields = ('title', 'featured', 'accessible', 'responsive_web', 'audience',)
 
 
 class ResourceList(generics.ListCreateAPIView):
