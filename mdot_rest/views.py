d7cfdbd2bde0cc876db8c1bce020d8a1cf0ea77b
Craig M. Stimmel <cstimmel@uw.edu>
2015-07-24 11:41:37 -0700

    Add search filtering for name and booleans in resource API.

--- mdot_rest/views.py
+++ mdot_rest/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from .models import Resource
 from .serializers import ResourceSerializer
 from rest_framework import generics, permissions
+import django_filters
+
+
+class ResourceFilter(django_filters.FilterSet):
+    class Meta:
+        model = Resource
+        fields = ('name', 'featured', 'accessible', 'responsive_web',)
 
 
 class ResourceList(generics.ListCreateAPIView):
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    filter_class = ResourceFilter
 
 
 class ResourceDetail(generics.RetrieveUpdateDestroyAPIView):
