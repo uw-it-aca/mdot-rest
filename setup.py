a0dc4b54247c35291dbf70eac3e483cd797452bf
Craig M. Stimmel <cstimmel@uw.edu>
2019-02-14 00:37:57 -0800

    Django 1.10 compatible.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'setuptools',
-        'django<1.9rc1',
+        'django<1.10,>1.9',
         'djangorestframework<3.7.0',
         'django-filter<2.0',
         'Pillow',
