91752581386e658ddac099db49343260cfb7cc75
erinRochfort99 <erochfor@uw.edu>
2020-06-03 14:18:19 -0700

    moved from django url to path

--- sampleproj/urls.py
+++ sampleproj/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/v1/', include('mdot_rest.urls')),
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('mdot_rest.urls')),
     # url(r'^', include('mdot.urls')),
 ]
 
