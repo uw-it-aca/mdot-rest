4eff42d5ae12aac50ba278f58f2669969d3ed918
erinRochfort99 <erochfor@uw.edu>
2020-06-08 14:18:38 -0700

    add admin to installed apps in travis settings

--- sampleproj/settings/travis.py
+++ sampleproj/settings/travis.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 from __future__ import absolute_import
 from .base import *
 
+INSTALLED_APPS += (
+    'django.contrib.admin',
+)
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'replaceme-xxxxxxxxxxxxxxxx'
