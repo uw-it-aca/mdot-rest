66f1e6ec7ced9ee44ecf07192bb7b149698a1d6e
erinRochfort99 <erochfor@uw.edu>
2020-09-18 15:26:42 -0700

    set up for django storages

--- docker/settings.py
+++ docker/settings.py
@@ -1,4 +1,5 @@
 from .base_settings import *
+from google.oauth2 import service_account
 import os
 
 if os.getenv('ENV', 'localdev') == 'localdev':
@@ -17,6 +18,15 @@ if DEBUG:
     MOCK_SAML_ATTRIBUTES['isMemberOf'] = ['u_test_admin']
     MEDIA_ROOT = '/app/'
     MEDIA_URL = '/media/'
+
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+    GS_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME', '')
+    GS_PROJECT_ID = os.getenv('STORAGE_PROJECT_ID', '')
+    GS_LOCATION = os.path.join(os.getenv('ENV'), 'media')
+    GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
+        '/gcs/credentials.json'
+    )
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
